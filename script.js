e146f5169149364817a6ab0997b5956282d49ce8
Keaton-A-Ramey <keaton.ramey@gmail.com>
2024-05-24 15:14:29 -0500

    quick change
    removed unused js and worked on cards

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const items = document.querySelectorAll('.timeline-item')
-
-    const isElementInViewport = (el) => {
-        const rect = el.getBoundingClientRect()
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        )
-    }
-
-    const run = () => {
-        items.forEach((item) => {
-            if (isElementInViewport(item)) {
-                item.classList.add('in-view')
-            }
-        })
-    }
-
-    window.addEventListener('load', run)
-    window.addEventListener('resize', run)
-    window.addEventListener('scroll', run)
-})
-
-document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.fade-in-card')
     let delay = 0
 
