2815e6a594fb6c0b6f5ba3498b1bb44f8ff4873e
Keaton-A-Ramey <93961620+Keaton-A-Ramey@users.noreply.github.com>
2024-08-16 17:40:20 -0500

    added spam filter page

---
+++ spam.js
@@ -0,0 +1,56 @@
+// Function to preprocess text
+function preprocessText(text) {
+    return text.toLowerCase().replace(/\W/g, ' '); // Convert to lowercase and remove non-word characters
+}
+
+// Increasing this decreases the likelihood message is flagged
+// Functions with values [-0.5, 0.5]
+// To make weight 0.7, for instance, make this 0.2
+const SPAM_WEIGHT = 0.0;
+
+async function loadJSON() {
+    const response = await fetch('data/data.json'); // Relative path to data.json
+    if (!response.ok) {
+        alert("Error getting JSON data.")
+        throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return await response.json();
+}
+
+function log(x) {
+    return x > 0 ? Math.log(x) : -Infinity; // To handle log(0)
+}
+
+async function detectSpam() {
+    // Prepare to assess email
+    const data = await loadJSON();
+    const message = document.getElementById("message-input").value;
+
+    wordsAndProbabilities = data.words_and_probabilities;
+    let hamProbability = log(data.ham_probability);
+    let spamProbability = log(data.spam_probability);
+
+    // Factor in all our words
+    const messageWords = preprocessText(message).split(' ');
+    for (const word of messageWords) {
+        if (wordsAndProbabilities[word]) {
+            const [wordSpam, wordHam] = wordsAndProbabilities[word];
+            spamProbability += log(wordSpam);
+            hamProbability += log(wordHam);
+        }
+    }
+
+    spamProbability -= SPAM_WEIGHT;
+
+    const resultElement = document.getElementById('result');
+    if (spamProbability > hamProbability) {
+        resultElement.textContent = "SPAM";
+    } else {
+        resultElement.textContent = "NOT SPAM";
+    }
+}
+
+// Attach detectSpam to button click
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('check-message').addEventListener('click', detectSpam);
+});
